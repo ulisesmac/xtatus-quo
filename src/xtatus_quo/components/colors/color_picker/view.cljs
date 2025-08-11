d6557e83975d3446b2947b631bc9d89b9d8350e5
Ulises M <ulises.ssb@hotmail.com>
2025-08-10 23:50:19 -0600

    Improve color picker implementation

--- src/xtatus_quo/components/colors/color_picker/view.cljs
+++ src/xtatus_quo/components/colors/color_picker/view.cljs
@@ -1,67 +1,90 @@
 (ns xtatus-quo.components.colors.color-picker.view
   (:require
-    [quo.components.colors.color.constants :as constants]
-    [quo.components.colors.color.view :as color]
-    [quo.foundations.colors :as colors]
-    [react-native.core :as rn]
-    [react-native.gesture :as gesture]))
+   [oops.core :as oops]
+   [quo.foundations.colors :as colors]
+   [react-native.core :as rn]
+   [reagent.core :as reagent]
+   [xtatus-quo.components.icon :as icons]))
 
-(defn get-item-layout
-  [_ index]
-  #js
-   {:length constants/color-size
-    :offset (* (+ constants/color-size 8) index)
-    :index  index})
+(defn color-selected-border [theme color]
+  [:rn/view {:style {:flex-direction :row
+                     :flex           1}}
+   [:rn/view {:style {:height                    48
+                      :flex                      1
+                      :background-color          (colors/resolve-color color theme 20)
+                      :border-top-left-radius    24
+                      :border-bottom-left-radius 24}}]
+   [:rn/view {:style {:height                     48
+                      :flex                       1
+                      :background-color           (colors/resolve-color color theme 40)
+                      :border-top-right-radius    24
+                      :border-bottom-right-radius 24}}]])
 
-(defn view
-  "Options
-   - `default-selected` Default selected color name.
-   - `on-change` Callback called when a color is selected `(fn [color-name])`.
-   - `blur?` Boolean to enable blur background support.}"
-  [{:keys [default-selected blur? on-change feng-shui? container-style]}]
-  (let [[selected set-selected] (rn/use-state default-selected)
-        {window-width :width}   (rn/get-window)
-        ref                     (rn/use-ref-atom nil)
-        on-ref                  (rn/use-callback #(reset! ref %))
-        render-fn               (rn/use-callback
-                                 (fn [color idx]
-                                   [color/view
-                                    {:selected?    (= color selected)
-                                     :on-press     (fn [color-name]
-                                                     (.scrollToIndex ^js @ref
-                                                                     #js
-                                                                      {:animated     true
-                                                                       :index        idx
-                                                                       :viewPosition 0.5})
-                                                     (set-selected color-name)
-                                                     (when on-change (on-change color-name)))
-                                     :blur?        blur?
-                                     :key          color
-                                     :color        color
-                                     :idx          idx
-                                     :window-width window-width}])
-                                 [selected blur? on-change @ref])]
-    (rn/use-mount
-     (fn []
-       (js/setTimeout
-        (fn []
-          (let [index (.indexOf colors/account-colors default-selected)]
-            (when (and @ref (>= index 0))
-              (some-> ^js @ref
-                      (.scrollToIndex #js
-                                       {:animated     false
-                                        :index        index
-                                        :viewPosition 0.5})))))
-        50)))
-    [gesture/flat-list
-     {:ref                               on-ref
-      ;; TODO: using :feng-shui? temporarily while b & w is being developed.
-      ;; https://github.com/status-im/status-mobile/discussions/16676
-      :data                              (if feng-shui?
-                                           (conj colors/account-colors :feng-shui)
-                                           colors/account-colors)
-      :render-fn                         render-fn
-      :get-item-layout                   get-item-layout
-      :horizontal                        true
-      :shows-horizontal-scroll-indicator false
-      :content-container-style           container-style}]))
+
+(defn color-item [{:keys [color selected set-selected! on-change ^js ref item-index]}]
+  (let [theme      (quo.context/use-theme)
+        selected?  (= @selected color)
+        on-select! (rn/use-callback
+                    (fn []
+                      (set-selected! color)
+                      (when (.. ref -current -scrollToIndex)
+                        (.. ref -current (scrollToIndex #js{:animated     true
+                                                            :index        item-index
+                                                            :viewPosition 0.5
+                                                            ;:viewOffset -48
+                                                            })))
+
+                      (on-change color))
+                    [])]
+    [:rn/pressable {:style    {:width          48
+                               :height         48
+                               :flex-direction :row}
+                    :on-press on-select!}
+     (when selected?
+       [color-selected-border theme color])
+     [:rn/view {:style {:position         :absolute
+                        :top              0
+                        :left             0
+                        :width            40
+                        :height           40
+                        :background-color (colors/resolve-color color theme)
+                        :border-radius    20
+                        :transform        [{:translate-x 4} {:translate-y 4}]
+                        :justify-content  :center
+                        :align-items      :center}}
+      (when selected?
+        [icons/icon :i/check {:size  20
+                              :color colors/white}])]]))
+
+(defn get-item-layout [_data index]
+  #js{:length 48
+      :offset (+ (* 48 index)
+                 (* 8 index) ;; spacing
+                 16) ;; left padding
+      :index  index})
+
+(defn view [{:keys [default-selected]}]
+  (let [selected      (reagent/atom default-selected)
+        set-selected! #(reset! selected %)]
+    (fn [{:keys [on-change]}]
+      (let [ref            (rn/use-ref nil)
+            render-item-fn (rn/use-callback
+                            (fn [js-data]
+                              (let [item (oops/oget js-data "item")]
+                                (reagent/as-element [color-item {:color         item
+                                                                 :selected      selected
+                                                                 :set-selected! set-selected!
+                                                                 :on-change     on-change
+                                                                 :ref           ref
+                                                                 :item-index    (.indexOf colors/account-colors item)}])))
+                            [on-change])]
+        [:gh/flat-list {:ref                               ref
+                        :content-container-style           {:column-gap 8
+                                                            :padding-vertical   8
+                                                            :padding-horizontal 16}
+                        :data                              (with-meta colors/account-colors {:keep-items true})
+                        :render-item                       render-item-fn
+                        :key-extractor                     str
+                        :get-item-layout                   get-item-layout
+                        :horizontal                        true
+                        :shows-horizontal-scroll-indicator false}]))))
