3b93a3000f137f68f274f7ddbee3227e982f4e94
Ulises M <ulises.ssb@hotmail.com>
2025-08-15 02:48:12 -0600

    Fix keyboard container

--- src/xtra_quo/components/emoji_picker.cljs
+++ src/xtra_quo/components/emoji_picker.cljs
@@ -5,6 +5,7 @@
    [oops.core :as oops]
    [quo.context]
    [quo.foundations.colors :as colors]
+   [quo.extra.components.above-keyboard-container.view :as above-keyboard]
    [react-native.core :as rn]
    [react-native.safe-area :as safe-area]
    [reagent.core :as reagent]
