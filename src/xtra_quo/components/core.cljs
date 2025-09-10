577048da61737e2eb434f2816e1780859efb3c1e
Ulises M <ulises.ssb@hotmail.com>
2025-09-10 14:06:28 -0600

    WIP: intermediate state

--- src/xtra_quo/components/core.cljs
+++ src/xtra_quo/components/core.cljs
@@ -1,10 +1,11 @@
 (ns xtra-quo.components.core
   (:require [quo.extra.components.above-keyboard-container.view]
-            [xtra-quo.components.emoji-picker]))
+   ;[xtra-quo.components.emoji-picker]
+            ))
 
 ;; Containers
 ;; TODO: move this component inside this folder
 ;; ;; TODO: add a blur layer
 (def above-keyboard-container quo.extra.components.above-keyboard-container.view/view)
 
-(def emoji-picker xtra-quo.components.emoji-picker/view)
+(def emoji-picker nil #_ xtra-quo.components.emoji-picker/view)
