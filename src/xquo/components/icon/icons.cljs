3a3af467666ba497196e96c5747de6b2d4af9bd7
Ulises M <ulises.ssb@hotmail.com>
2026-03-05 00:03:00 -0600

    Implement settings item

--- src/xquo/components/icon/icons.cljs
+++ src/xquo/components/icon/icons.cljs
@@ -1,7 +1,8 @@
-(ns xquo.components.icon.icons
+(ns ^:dev/always xquo.components.icon.icons
   (:require-macros [xquo.components.icon.icons :as icons]))
 
 ;; Resolved at compile time from resources/icons/required.edn.
+;; Update this namespace when required.edn changes so newly used icons are included.
 (def ^:private icons (icons/resolve-icons))
 
 (defn icon-source [icon-key]
--- src/xquo/core.cljs
+++ src/xquo/core.cljs
@@ -4,6 +4,7 @@
             [xquo.components.counter.step.view]
             [xquo.components.icon.view]
             [xquo.components.page-nav.view]
+            [xquo.components.settings.item.view]
             [xquo.components.settings.sort-item.view]
             [xquo.components.selectors.disclaimer.view]
             [xquo.components.selectors.filter.view]
@@ -16,6 +17,7 @@
 (def page-nav xquo.components.page-nav.view/page-nav)
 (def page-nav-left-action xquo.components.page-nav.view/nav-left-action)
 (def page-nav-title xquo.components.page-nav.view/nav-title)
+(def settings-item xquo.components.settings.item.view/settings-item)
 (def sort-item xquo.components.settings.sort-item.view/sort-item)
 (def disclaimer xquo.components.selectors.disclaimer.view/disclaimer)
 (def filter xquo.components.selectors.filter.view/filter)
