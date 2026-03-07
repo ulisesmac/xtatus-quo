837c438ba3272f12b667be0a9c5be40c1c5b7253
Ulises M <ulises.ssb@hotmail.com>
2026-03-06 19:29:17 -0600

    Add info message component

---
+++ src/xquo/components/info/info_message/style.cljs
@@ -0,0 +1,48 @@
+(ns xquo.components.info.info-message.style
+  (:require [reagent-extended-compiler.utils.transforms :refer [defstyle]]
+            [xquo.foundations.colors :as colors]))
+
+(defstyle container-base
+  {:flex-direction :row
+   :gap            4})
+
+(defstyle default-size-base
+  {:align-items :flex-start})
+
+(defstyle tiny-size-base
+  {:align-items :center})
+
+(defstyle text-slot
+  {:flex      1
+   :min-width 1})
+
+(defstyle icon-default
+  {:padding-top    1
+   :padding-bottom 1})
+
+(defstyle icon-tiny
+  {:padding-top    2
+   :padding-bottom 2})
+
+(defn message-color [theme background status]
+  {:color (cond
+            (= status :success)
+            (colors/get-color :color/success
+                              (if (= theme :theme/dark) 60 50))
+
+            (= status :error)
+            (colors/get-color :color/danger
+                              (if (= theme :theme/dark) 60 50))
+
+            (= status :warning)
+            (colors/get-color :color/orange
+                              (if (= theme :theme/dark) 60 50))
+
+            (and (= theme :theme/dark) (= background :blur))
+            (colors/get-color :color/white 40)
+
+            (= theme :theme/dark)
+            (colors/get-color :color/neutral 40)
+
+            :else
+            (colors/get-color :color/neutral 50))})
---
+++ src/xquo/components/info/info_message/view.cljs
@@ -0,0 +1,48 @@
+(ns xquo.components.info.info-message.view
+  (:require [reagent-extended-compiler.utils.transforms :as rec.xf]
+            [xquo.components.info.info-message.style :as style]
+            [xquo.components.icon.view :as icon]
+            [xquo.components.text.view :as text]
+            [xquo.context :as context]))
+
+(defn info-message
+  "Feedback info message.
+
+  API:
+  - `props` map
+    - `:status` one of `:default`, `:success`, `:error`, `:warning`
+      (default `:default`)
+    - `:size` one of `:default`, `:tiny` (default `:default`)
+    - `:background` one of `:none`, `:blur` (default `:none`)
+    - `:style` optional caller style (map/vector/js style)
+    - Any additional keys are forwarded to `:rn/view`.
+  - `content` message text."
+  [{:keys [status size background]
+    :or   {status     :default
+           size       :default
+           background :none}
+    :as   props}
+   content]
+  (let [theme               (context/use-theme)
+        message-color-style (style/message-color theme background status)
+        tiny?               (= size :tiny)]
+    [:rn/view (-> props
+                  (dissoc :status :size :background :style)
+                  (assoc :style (rec.xf/add-styles
+                                 style/container-base
+                                 (if tiny?
+                                   style/tiny-size-base
+                                   style/default-size-base)
+                                 (:style props))))
+     (if tiny?
+       [icon/icon {:icon  :icon/info
+                   :size  12
+                   :color (:color message-color-style)
+                   :style style/icon-tiny}]
+       [icon/icon {:icon  :icon/info
+                   :size  16
+                   :color (:color message-color-style)
+                   :style style/icon-default}])
+     [text/text {:font  (if tiny? :font/regular-11 :font/regular-13)
+                 :style [style/text-slot message-color-style]}
+      content]]))
