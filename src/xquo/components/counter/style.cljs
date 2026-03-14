69b91984d81374ae7f49060764d46de806634192
Ulises M <ulises.ssb@hotmail.com>
2026-03-14 00:24:52 -0600

    Add counter component

---
+++ src/xquo/components/counter/style.cljs
@@ -0,0 +1,194 @@
+(ns xquo.components.counter.style
+  (:require-macros [reagent-extended-compiler.utils.transforms :refer [defstyle style]])
+  (:require [xquo.foundations.colors :as colors]))
+
+(def ^:private default-radius 6)
+(def ^:private large-radius 8)
+
+(def ^:private default-widths
+  {:one               20
+   :two               20
+   :three             27
+   :ninety-nine-plus  28
+   :negative          28
+   :four              34})
+
+(def ^:private large-layouts
+  {:one               {:width 20 :padding-left 5.5 :padding-right 5.5}
+   :two               {:width 23 :padding-left 4 :padding-right 4}
+   :three             {:width 34 :padding-left 4.5 :padding-right 4.5}
+   :ninety-nine-plus  {:width 32 :padding-left 4 :padding-right 2}
+   :negative          {:width 32 :padding-left 4 :padding-right 5}
+   :four              {:width 42 :padding-left 4.5 :padding-right 4.5}})
+
+(def ^:private default-value-slot-insets
+  {:one               {:top 2 :right 2 :bottom 2 :left 2}
+   :two               {:top 2 :right 0 :bottom 2 :left 0}
+   :three             {:top 2 :right 2 :bottom 2 :left 2}
+   :ninety-nine-plus  {:top 2 :right 2 :bottom 2 :left 3}
+   :negative          {:top 2 :right 3 :bottom 2 :left 2}
+   :four              {:top 2 :right 0 :bottom 2 :left 0}})
+
+(defstyle default-root-base
+  {:height        20
+   :overflow      :hidden
+   :position      :relative
+   :border-radius default-radius})
+
+(defstyle default-surface-base
+  {:position      :absolute
+   :border-radius default-radius})
+
+(defstyle default-value-slot-base
+  {:position        :absolute
+   :justify-content :center
+   :align-items     :center})
+
+(defstyle large-root-base
+  {:height           20
+   :overflow         :hidden
+   :flex-direction   :row
+   :justify-content  :center
+   :align-items      :center
+   :border-radius    large-radius})
+
+(defn- outline-border-color [color dark-theme? blur?]
+  (if (and dark-theme? blur?)
+    (colors/get-color :color/white 10)
+    (colors/get-color color 50 20)))
+
+(defn- outline-text-color [color dark-theme? blur?]
+  (cond
+    (and dark-theme? blur?) (colors/get-color :color/white 40)
+    dark-theme?            (colors/get-color color 60)
+    :else                  (colors/get-color color 50)))
+
+(defn- default-type-style [color type dark-theme? blur?]
+  (case type
+    :default
+    {:background-color (cond
+                         (and dark-theme? blur?) (colors/get-color :color/blue 50)
+                         dark-theme?            (colors/get-color :color/blue 60)
+                         :else                  (colors/get-color :color/blue 50))}
+
+    :secondary
+    {:background-color (if dark-theme?
+                         (colors/get-color :color/white 5)
+                         (colors/get-color :color/neutral 80 5))}
+
+    :grey
+    {:background-color (cond
+                         (and dark-theme? blur?) (colors/get-color :color/white 10)
+                         dark-theme?            (colors/get-color :color/neutral 80)
+                         blur?                  (colors/get-color :color/neutral 80 10)
+                         :else                  (colors/get-color :color/neutral 10))}
+
+    :outline
+    {:border-width 1
+     :border-color (cond
+                     (and dark-theme? blur?) (colors/get-color :color/white 10)
+                     dark-theme?            (colors/get-color :color/neutral 80)
+                     blur?                  (colors/get-color :color/neutral 80 5)
+                     :else                  (colors/get-color :color/neutral 20))}
+
+    :outline-color
+    {:border-width 1
+     :border-color (outline-border-color color dark-theme? blur?)}
+
+    :warning
+    {:background-color (colors/get-color :color/warning 50 10)
+     :border-width     1
+     :border-color     (colors/get-color :color/warning 50 20)}
+
+    :error
+    {:background-color (colors/get-color :color/danger 50 10)
+     :border-width     1
+     :border-color     (colors/get-color :color/danger 50 20)}
+
+    {:background-color (cond
+                         (and dark-theme? blur?) (colors/get-color :color/blue 50)
+                         dark-theme?            (colors/get-color :color/blue 60)
+                         :else                  (colors/get-color :color/blue 50))}))
+
+(defn- large-type-style [color type dark-theme? blur?]
+  (case type
+    :default
+    {:background-color (if dark-theme?
+                         (colors/get-color :color/blue 60)
+                         (colors/get-color :color/blue 50))}
+
+    :secondary
+    {:background-color (if dark-theme?
+                         (colors/get-color :color/white 5)
+                         (colors/get-color :color/neutral 80 5))}
+
+    :grey
+    {:background-color (cond
+                         (and dark-theme? blur?) (colors/get-color :color/white 10)
+                         dark-theme?            (colors/get-color :color/neutral 80)
+                         blur?                  (colors/get-color :color/neutral 80 10)
+                         :else                  (colors/get-color :color/neutral 10))}
+
+    :outline
+    {:border-width 1
+     :border-color (cond
+                     (and dark-theme? blur?) (colors/get-color :color/white 10)
+                     dark-theme?            (colors/get-color :color/neutral 80)
+                     blur?                  (colors/get-color :color/neutral 80 5)
+                     :else                  (colors/get-color :color/neutral 20))}
+
+    :outline-color
+    {:border-width 1
+     :border-color (outline-border-color color dark-theme? blur?)}
+
+    :warning
+    {:background-color (colors/get-color :color/warning 50 10)
+     :border-width     1
+     :border-color     (colors/get-color :color/warning 50 20)}
+
+    :error
+    {:background-color (colors/get-color :color/danger 50 10)
+     :border-width     1
+     :border-color     (colors/get-color :color/danger 50 20)}
+
+    {:background-color (if dark-theme?
+                         (colors/get-color :color/blue 60)
+                         (colors/get-color :color/blue 50))}))
+
+(defn default-root-style [layout-key type dark-theme? blur?]
+  (style {:width            (get default-widths layout-key)
+          :background-color (when (and (= type :default) dark-theme? blur?)
+                              (colors/get-color :color/blue 60))}))
+
+(defn default-surface-style [color layout-key type dark-theme? blur?]
+  (let [horizontal-inset (if (= layout-key :one) 2 0)]
+    (style (merge {:top    2
+                   :right  horizontal-inset
+                   :bottom 2
+                   :left   horizontal-inset}
+                  (default-type-style color type dark-theme? blur?)))))
+
+(defn default-value-slot-style [layout-key]
+  (style (get default-value-slot-insets layout-key)))
+
+(defn large-root-style [color layout-key type dark-theme? blur?]
+  (let [{:keys [width padding-left padding-right]} (get large-layouts layout-key)]
+    (style (merge {:width         width
+                   :padding-left  padding-left
+                   :padding-right padding-right}
+                  (large-type-style color type dark-theme? blur?)))))
+
+(defn value-text-style [color type dark-theme? blur?]
+  (style {:color     (case type
+                       :default       (colors/get-color :color/white 100)
+                       :outline-color (outline-text-color color dark-theme? blur?)
+                       :warning       (if dark-theme?
+                                        (colors/get-color :color/warning 60)
+                                        (colors/get-color :color/warning 50))
+                       :error         (if dark-theme?
+                                        (colors/get-color :color/danger 60)
+                                        (colors/get-color :color/danger 50))
+                       (if dark-theme?
+                         (colors/get-color :color/white 100)
+                         (colors/get-color :color/neutral 100)))
+          :text-align :center}))
---
+++ src/xquo/components/counter/view.cljs
@@ -0,0 +1,93 @@
+(ns xquo.components.counter.view
+  (:require [reagent-extended-compiler.utils.transforms :as rec.xf]
+            [xquo.components.counter.style :as counter-style]
+            [xquo.components.text.view :as text]
+            [xquo.context :as context]))
+
+(def ^:private value-font
+  {:default :font/medium-11
+   :large   :font/medium-13})
+
+(defn- value-layout-key [label]
+  (cond
+    (= label "99+")      :ninety-nine-plus
+    (= label "-99")      :negative
+    (<= (count label) 1) :one
+    (= (count label) 2)  :two
+    (= (count label) 3)  :three
+    :else                :four))
+
+(defn- value-view [{:keys [blur? color dark-theme? size type]} label]
+  [text/text {:font            (get value-font size)
+              :number-of-lines 1
+              :style           (counter-style/value-text-style color type dark-theme? blur?)}
+   label])
+
+(defn- default-counter-view [{:keys [blur? color dark-theme? root-props style type value]}]
+  (let [label      (str value)
+        layout-key (value-layout-key label)]
+    [:rn/view (assoc root-props :style (rec.xf/add-styles
+                                        counter-style/default-root-base
+                                        (counter-style/default-root-style layout-key type dark-theme? blur?)
+                                        style))
+     [:rn/view {:style [counter-style/default-surface-base
+                        (counter-style/default-surface-style color layout-key type dark-theme? blur?)]}]
+     [:rn/view {:style [counter-style/default-value-slot-base
+                        (counter-style/default-value-slot-style layout-key)]}
+      [value-view {:blur?       blur?
+                   :color       color
+                   :dark-theme? dark-theme?
+                   :size        :default
+                   :type        type}
+       label]]]))
+
+(defn- large-counter-view [{:keys [blur? color dark-theme? root-props style type value]}]
+  (let [label      (str value)
+        layout-key (value-layout-key label)]
+    [:rn/view (assoc root-props :style (rec.xf/add-styles
+                                        counter-style/large-root-base
+                                        (counter-style/large-root-style color layout-key type dark-theme? blur?)
+                                        style))
+     [value-view {:blur?       blur?
+                  :color       color
+                  :dark-theme? dark-theme?
+                  :size        :large
+                  :type        type}
+      label]]))
+
+(defn counter
+  "Counter component.
+
+  API:
+  - `props` map
+    - `:type` one of `:default`, `:secondary`, `:grey`, `:outline`,
+      `:outline-color`, `:warning`, `:error` (default `:default`)
+    - `:size` one of `:default` or `:large` (default `:default`)
+    - `:blur?` optional boolean that uses the blur treatment (default `false`)
+    - `:style` optional caller style (map/vector/js style)
+    - Any additional keys are forwarded to the root `:rn/view`
+      (for example `:accessibility-label`, `:testID`)
+  - `value` label rendered inside the counter."
+  [{:keys [blur? size style type]
+    :or   {blur? false
+           size  :default
+           type  :default}
+    :as   props}
+   value]
+  (let [{:keys [color dark-theme?]} (context/use-theme-color)
+        root-props            (dissoc props :blur? :size :style :type)]
+    (if (= size :large)
+      [large-counter-view {:blur?       blur?
+                           :color       color
+                           :dark-theme? dark-theme?
+                           :root-props  root-props
+                           :style       style
+                           :type        type
+                           :value       value}]
+      [default-counter-view {:blur?       blur?
+                             :color       color
+                             :dark-theme? dark-theme?
+                             :root-props  root-props
+                             :style       style
+                             :type        type
+                             :value       value}])))
