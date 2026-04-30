fc93ad702df05be5bbb89f6bcfdf5ce64c19b8da
Ulises M <ulises.ssb@hotmail.com>
2026-04-30 14:08:51 -0600

    Add divider-label component

---
+++ src/xquo/components/divider/divider_label/style.cljs
@@ -0,0 +1,98 @@
+(ns xquo.components.divider.divider-label.style
+  (:require [reagent-extended-compiler.utils.transforms :refer [defstyle style]]
+            [xquo.components.button.style :as button-style]
+            [xquo.components.settings.item.style :as settings-item-style]
+            [xquo.foundations.animations :as animations]
+            [xquo.foundations.colors :as colors]
+            [xquo.react-native :as rn]))
+
+(defstyle container-base
+  {:width            "100%"
+   :flex-direction   :row
+   :align-items      :center
+   :overflow         :hidden
+   :position         :relative
+   :border-top-width 1
+   :padding-top      15
+   :padding-bottom   8
+   :padding-left     20
+   :padding-right    20})
+
+(defstyle compact-container
+  {:padding-top 7})
+
+(defstyle right-chevron-container
+  {:padding-left 16})
+
+(defstyle title-slot
+  {:flex       1
+   :min-width  1
+   :min-height 1})
+
+(defstyle left-chevron-slot
+  {:width           20
+   :height          20
+   :align-items     :center
+   :justify-content :center
+   :margin-right    12})
+
+(defstyle right-chevron-slot
+  {:width           20
+   :height          20
+   :align-items     :center
+   :justify-content :center
+   :margin-left     2})
+
+(defn chevron-state [open? toggle-duration toggle-timing-function]
+  (style {:transform                  [{:rotate (if open? "0deg" "-90deg")}]
+          :transition-property        "transform"
+          :transition-duration        toggle-duration
+          :transition-timing-function toggle-timing-function}))
+
+(defstyle counter-slot
+  {:width           20
+   :height          20
+   :align-items     :center
+   :justify-content :center
+   :margin-left     2})
+
+(defstyle right-slot
+  {:min-width       20
+   :height          20
+   :align-items     :flex-end
+   :justify-content :center
+   :margin-left     2})
+
+(def overlay-base rn/style-sheet-absolute-fill)
+
+(defn overlay-color [theme blur?]
+  (if blur?
+    (settings-item-style/pressed-overlay-color-style theme blur?)
+    (button-style/pressable-type-style theme :ghost :none :color/primary false true)))
+
+(defn overlay-state [pressed?]
+  (style {:opacity                    (if pressed? 1 0)
+          :transition-property        "opacity"
+          :transition-duration        (if pressed?
+                                       (:pressed-duration animations/press-feedback)
+                                       (:default-duration animations/press-feedback))
+          :transition-timing-function (if pressed?
+                                       (:pressed-timing-function animations/press-feedback)
+                                       (:default-timing-function animations/press-feedback))}))
+
+(defn title-color [theme blur?]
+  {:color (cond
+            (and (= theme :theme/dark) blur?)  (colors/get-color :color/white-70)
+            (and (= theme :theme/light) blur?) (colors/get-color :color/neutral-80-70)
+            (= theme :theme/dark)              (colors/get-color :color/neutral-40)
+            :else                              (colors/get-color :color/neutral-50))})
+
+(defn border-color [theme blur?]
+  {:border-top-color (cond
+                       (and (= theme :theme/dark) blur?)  (colors/get-color :color/white-5)
+                       (and (= theme :theme/light) blur?) (colors/get-color :color/neutral-80-5)
+                       (= theme :theme/dark)              (colors/get-color :color/neutral-90)
+                       :else                              (colors/get-color :color/neutral-10))})
+
+(defn icon-color [theme blur?]
+  (:color (title-color theme blur?)))
---
+++ src/xquo/components/divider/divider_label/view.cljs
@@ -0,0 +1,136 @@
+(ns xquo.components.divider.divider-label.view
+  (:require [reagent-extended-compiler.utils.transforms :as rec.xf]
+            [xquo.components.counter.view :as counter]
+            [xquo.components.divider.divider-label.style :as style]
+            [xquo.components.icon.view :as icon]
+            [xquo.components.text.view :as text]
+            [xquo.context :as context]
+            [xquo.foundations.animations :as animations]
+            [xquo.react-native :as rn]))
+
+(defn- chevron-view [{:keys [theme blur? open? toggle-duration toggle-timing-function]}]
+  [:animated/view {:style (style/chevron-state open? toggle-duration toggle-timing-function)}
+   [icon/view {:name  :icon/chevron-down
+               :size  20
+               :color (style/icon-color theme blur?)}]])
+
+(defn- right-view [{:keys [blur? counter right]}]
+  [:rn/view {:style (if right style/right-slot style/counter-slot)}
+   (if right
+     right
+     [counter/counter {:type  :grey
+                       :blur? blur?}
+      counter])])
+
+(defn- content-view [{:keys [theme blur? chevron-side counter open? right title toggle-duration
+                             toggle-timing-function]}]
+  [:<>
+   (when (= chevron-side :left)
+     [:rn/view {:style style/left-chevron-slot}
+      [chevron-view {:theme theme
+                     :blur? blur?
+                     :open? open?
+                     :toggle-duration toggle-duration
+                     :toggle-timing-function toggle-timing-function}]])
+   [:rn/view {:style style/title-slot}
+    [text/text {:font            :font/medium-13
+                :number-of-lines 1
+                :style           (style/title-color theme blur?)}
+     title]]
+   (when (= chevron-side :right)
+     [:rn/view {:style style/right-chevron-slot}
+      [chevron-view {:theme theme
+                     :blur? blur?
+                     :open? open?
+                     :toggle-duration toggle-duration
+                     :toggle-timing-function toggle-timing-function}]])
+   (when (or right counter)
+     [right-view {:blur?   blur?
+                  :counter counter
+                  :right   right}])])
+
+(defn divider-label
+  "Divider label component.
+
+  API:
+  - `props` map
+    - `:title` label text
+    - `:initial-open?` optional initial open state (default `false`)
+    - `:compact?` optional compact vertical spacing (default `true`)
+    - `:blur?` optional boolean for blur styling
+    - `:chevron` optional `:left` or `:right`
+    - `:counter` optional right counter value. Forces the chevron to the left.
+    - `:right` optional custom right-side hiccup in the counter slot.
+    - `:style` optional caller style (map/vector/js style)
+    - `:on-press` optional press callback
+    - `:on-press-in` optional press-in callback
+    - `:on-press-out` optional press-out callback
+    - `:disabled?` optional disabled state for pressable labels
+    - `:open?` optional controlled open state
+    - `:toggle-duration` optional chevron animation duration
+    - `:toggle-timing-function` optional chevron animation timing function
+    - Any additional keys are forwarded to the root container."
+  [{controlled-open? :open?
+    :keys            [title initial-open? compact? blur? chevron counter right on-press
+                      on-press-in on-press-out disabled? toggle-duration toggle-timing-function]
+    :or   {compact?               true
+           initial-open?          false
+           toggle-duration        (:toggle-duration animations/state-change)
+           toggle-timing-function (:transition-timing-function animations/state-change)}
+    :as   props}]
+  (let [theme                   (context/use-theme)
+        chevron-side            (if counter :left chevron)
+        pressable               (or on-press chevron-side)
+        [local-open? set-open!] (rn/use-state initial-open?)
+        [pressed? set-pressed!] (rn/use-state false)
+        controlled?             (some? controlled-open?)
+        open?                   (if controlled? controlled-open? local-open?)
+        on-press!               (rn/use-callback
+                                  (fn [event]
+                                    (when-not controlled?
+                                      (set-open! not))
+                                    (when on-press
+                                      (on-press event)))
+                                  [controlled? on-press])
+        on-press-in!            (rn/use-callback
+                                  (fn [event]
+                                    (set-pressed! true)
+                                    (when on-press-in
+                                      (on-press-in event)))
+                                  [on-press-in])
+        on-press-out!           (rn/use-callback
+                                  (fn [event]
+                                    (set-pressed! false)
+                                    (when on-press-out
+                                      (on-press-out event)))
+                                  [on-press-out])
+        root-component          (if pressable :rn/pressable :rn/view)]
+    [root-component
+     (-> props
+         (dissoc :title :initial-open? :compact? :blur? :chevron :counter :right :style
+                 :on-press :on-press-in :on-press-out :disabled? :open? :toggle-duration
+                 :toggle-timing-function :layout :entering :exiting)
+         (assoc :style (rec.xf/add-styles
+                        style/container-base
+                        (style/border-color theme blur?)
+                        (when compact? style/compact-container)
+                        (when (= chevron-side :right) style/right-chevron-container)
+                        (:style props)))
+         (cond-> pressable (assoc :disabled     disabled?
+                                  :on-press     on-press!
+                                  :on-press-in  on-press-in!
+                                  :on-press-out on-press-out!)))
+     (when pressable
+       [:animated/view {:pointer-events :none
+                        :style          [style/overlay-base
+                                         (style/overlay-color theme blur?)
+                                         (style/overlay-state pressed?)]}])
+     [content-view {:theme           theme
+                    :blur?           blur?
+                    :chevron-side    chevron-side
+                    :counter         counter
+                    :open?           open?
+                    :right           right
+                    :title           title
+                    :toggle-duration toggle-duration
+                    :toggle-timing-function toggle-timing-function}]]))
