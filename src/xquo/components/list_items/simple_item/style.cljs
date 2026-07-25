def1fc2d3c580b71433a076e291ff000cd386c43
Ulises M <ulises.ssb@hotmail.com>
2026-07-24 23:08:17 -0600

    WIP: suspiocious cleanup

---
+++ src/xquo/components/list_items/simple_item/style.cljs
@@ -0,0 +1,58 @@
+(ns xquo.components.list-items.simple-item.style
+  (:require [react-native.utils :refer [defstyle style]]
+            [xquo.foundations.borders :as borders]
+            [xquo.foundations.colors :as colors]))
+
+(defstyle container-base
+  {:align-self    :stretch
+   :position      :relative
+   :overflow      :hidden
+   :border-radius (borders/radius 40)})
+
+(defstyle row
+  {:flex-direction     :row
+   :align-items        :center
+   :column-gap         12
+   :padding-horizontal 12
+   :padding-vertical   8
+   :z-index            1})
+
+(defstyle leading
+  {:width           32
+   :height          32
+   :flex-shrink     0
+   :align-items     :center
+   :justify-content :center
+   :border-radius   (borders/radius :max)})
+
+(defstyle emoji
+  {:width                20
+   :height               20
+   :font-size            20
+   :line-height          20
+   :text-align           :center
+   :text-align-vertical  :center
+   :include-font-padding false})
+
+(defstyle title
+  {:flex      1
+   :min-width 0})
+
+(defstyle right
+  {:width           20
+   :height          20
+   :flex-shrink     0
+   :align-items     :center
+   :justify-content :center})
+
+(def leading-icon-color
+  (colors/get-color :color/neutral-80-70))
+
+(def icon-background-color
+  (colors/get-color :color/neutral-80-5))
+
+(def emoji-background-color
+  (colors/get-color :color/primary-50-10))
+
+(defn leading-background [background-color]
+  (style {:background-color background-color}))
---
+++ src/xquo/components/list_items/simple_item/view.cljs
@@ -0,0 +1,63 @@
+(ns xquo.components.list-items.simple-item.view
+  (:require [react-native.core :as rn]
+            [react-native.utils :as rn.utils]
+            [xquo.components.icon.view :as icon]
+            [xquo.components.list-items.simple-item.style :as style]
+            [xquo.components.list.style :as list.style]
+            [xquo.components.text.view :as text]))
+
+(defn- leading-view [{:keys [background-color emoji icon]}]
+  [:rn/view {:style [style/leading (style/leading-background background-color)]}
+   (if emoji
+     [:rn/text {:style style/emoji}
+      emoji]
+     [icon/view (assoc icon
+                  :size  20
+                  :color (:color icon style/leading-icon-color))])])
+
+(defn simple-item
+  "Simple list item with optional circular leading content and a trailing slot.
+
+  Pass either `:icon` or `:emoji`. Use `:leading-background-color` to override
+  the content-based default with a resolved color value."
+  [{:keys [emoji icon leading-background-color on-press-in on-press-out right title]
+    :as   props}]
+  (let [[pressed? set-pressed!] (rn/use-state false)
+        on-press-in!           (rn/use-callback
+                                (fn [event]
+                                  (set-pressed! true)
+                                  (when on-press-in
+                                    (on-press-in event)))
+                                [on-press-in])
+        on-press-out!          (rn/use-callback
+                                (fn [event]
+                                  (set-pressed! false)
+                                  (when on-press-out
+                                    (on-press-out event)))
+                                [on-press-out])]
+    [:rn/pressable (-> props
+                       (dissoc :emoji :icon :leading-background-color :on-press-in :on-press-out :right :style :title)
+                       (assoc :on-press-in  on-press-in!
+                              :on-press-out on-press-out!
+                              :style        (rn.utils/add-styles
+                                             style/container-base
+                                             list.style/pressable-element-spacing
+                                             (:style props))))
+     [:animated/view {:pointer-events :none
+                      :style          [list.style/overlay-base
+                                       (list.style/pressed-color-style :color/primary)
+                                       (list.style/pressed-color-state-style pressed?)]}]
+     [:rn/view {:style style/row}
+      (when (or (:name icon) emoji)
+        [leading-view {:background-color (or leading-background-color
+                                             (if emoji
+                                               style/emoji-background-color
+                                               style/icon-background-color))
+                       :emoji            emoji
+                       :icon             icon}])
+      [:rn/view {:style style/title}
+       [text/text {:font :font/medium-15}
+        title]]
+      (when right
+        [:rn/view {:style style/right}
+         right])]]))
