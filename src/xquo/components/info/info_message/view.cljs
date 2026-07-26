dcbef540fd5b9e87f2e90cbdde4f9ce7b57a69d8
Ulises M <ulises.ssb@hotmail.com>
2026-07-25 18:43:57 -0600

    Allow info message to be color customizable

--- src/xquo/components/info/info_message/view.cljs
+++ src/xquo/components/info/info_message/view.cljs
@@ -14,20 +14,21 @@
       (default `:default`)
     - `:size` one of `:default`, `:tiny` (default `:default`)
     - `:background` one of `:none`, `:blur` (default `:none`)
+    - `:color` optional message text and icon color override
     - `:style` optional caller style (map/vector/js style)
     - Any additional keys are forwarded to `:rn/view`.
   - `content` message text."
-  [{:keys [status size background]
+  [{:keys [status size background color]
     :or   {status     :default
            size       :default
            background :none}
     :as   props}
    content]
-  (let [theme               (context/use-theme)
-        message-color-style (style/message-color theme background status)
-        tiny?               (= size :tiny)]
+  (let [theme         (context/use-theme)
+        content-color (or color (:color (style/message-color theme background status)))
+        tiny?         (= size :tiny)]
     [:rn/view (-> props
-                  (dissoc :status :size :background :style)
+                  (dissoc :status :size :background :color :style)
                   (assoc :style (rn.utils/add-styles
                                  style/container-base
                                  (if tiny?
@@ -37,12 +38,12 @@
      (if tiny?
        [icon/view {:name  :icon/info-outline
                    :size  12
-                   :color (:color message-color-style)
+                   :color content-color
                    :style style/icon-tiny}]
        [icon/view {:name  :icon/info-outline
                    :size  16
-                   :color (:color message-color-style)
+                   :color content-color
                    :style style/icon-default}])
      [text/text {:font  (if tiny? :font/regular-11 :font/regular-13)
-                 :style [style/text-slot message-color-style]}
+                 :style [style/text-slot {:color content-color}]}
       content]]))
